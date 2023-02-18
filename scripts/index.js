198cc832ed76c61253eacbcc5d76acd7990f5ce3
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-02-18 13:38:59 +0300

    refactoring: decomposed cards array

--- scripts/index.js
+++ scripts/index.js
@@ -34,38 +34,7 @@ const templatePlaceItem = document.getElementById(
 ).content;
 const cardsContainer = document.querySelector('.place__list');
 
-const initialPlaceCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-    alt: 'Горная река Архыза ранней весной',
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-    alt: 'Лесное озеро ранней весной',
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-    alt: 'Панельные дома',
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-    alt: 'Почва Камчатки',
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-    alt: 'Железная дорога идет сквозь лес',
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-    alt: 'Байкал во льду',
-  },
-];
+
 
 // common
 
