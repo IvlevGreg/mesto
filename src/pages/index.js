172a3209db6be6ac5d72788e1ebece01f2d24a33
Ivlev Grigoriy <igs94@mail.ru>
2023-03-16 21:49:04 +0300

    feat: add webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
-import { Card } from './components/Card.js';
-import { initialPlaceCards } from './utils/cardsArray.js';
-import { FormValidator } from './components/FormValidator.js';
-import { Section } from './components/Section.js';
-import { PopupWithForm } from './components/PopupWithForm.js';
-import { PopupWithImage } from './components/PopupWithImage.js';
-import { UserInfo } from './components/UserInfo.js';
+import './index.css';
+
+import { Card } from '../scripts/components/Card.js';
+import { initialPlaceCards } from '../scripts/utils/cardsArray.js';
+import { FormValidator } from '../scripts/components/FormValidator.js';
+import { Section } from '../scripts/components/Section.js';
+import { PopupWithForm } from '../scripts/components/PopupWithForm.js';
+import { PopupWithImage } from '../scripts/components/PopupWithImage.js';
+import { UserInfo } from '../scripts/components/UserInfo.js';
 
 const buttonEdit = document.querySelector('.profile__edit-button');
 const formEdit = document.querySelector('.popup-form_edit');
